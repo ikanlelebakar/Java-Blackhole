d30b4b9eab3c7213c0c12574df001bd1c7697306
fish <faishalabdullah13@gmail.com>
2025-10-27 21:41:24 +0700

    First coding

--- src/main.java
+++ src/main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class main {
+    public static double Lingkaran(double jariJari) {
+        final double Pi = 3.14159265358979323846f;
+
+        return Pi * Math.pow(jariJari, 2);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //deklarasi Variabel, scanner dan kelas
+        double jariJari;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Jari Jari : ");
+        jariJari = input.nextDouble();
+        System.out.print("Luas lingkaran ini = " + Lingkaran(jariJari));
     }
 }
